35441efe4396487dc2c38bafd53f8d3537bf9db7
EmediongEdet <blessededet94@gmail.com>
2025-03-13 15:23:15 +0100

    Created a create instructor page

--- client/src/components/CreateInstructor.jsx
+++ client/src/components/CreateInstructor.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateInstructor = () => {
+    const [name, setName] = useState('');
+    const [course, setCourse] = useState('')
   return (
-    <div>
-      
-    </div>
+    <section className='create__instructor'>
+        <div className='container'>
+            <h2>Create an Instructor</h2>
+            <p className='form__error-message'>
+                This is an error message
+            </p>
+            <form className='form create-instructor__form'>
+                <input type='text' placeholder='Full Name'></input>
+            </form>
+        </div>
+    </section>
   )
 }
 
