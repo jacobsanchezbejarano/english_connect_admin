9a45e3e25abac648f131220297f878f3d078d290
EmediongEdet <blessededet94@gmail.com>
2025-04-02 13:09:44 +0100

    updated added

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Groups = () => {
   const groups = [
-    { name: 'English Connect 1', description: 'Beginner level course focused on basic communication skills.' },
-    { name: 'English Connect 2', description: 'Intermediate level course aimed at enhancing fluency and comprehension.' },
-    { name: 'English Connect 3', description: 'Advanced level course designed to refine language skills and accuracy.' }
+    { name: 'English Connect 1', description: 'Beginner level courses focused on basic communication skills.' },
+    { name: 'English Connect 2', description: 'Intermediate level courses aimed at enhancing fluency and comprehension.' },
+    { name: 'English Connect 3', description: 'Advanced level courses designed to refine language skills and accuracy.' }
   ];
 
   return (
