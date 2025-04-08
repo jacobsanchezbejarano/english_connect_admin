057aa4f2dd51ca0c3f92b00e90a233ebaf86f2c3
Cesar Coutino <cou23001@byui.edu>
2025-04-08 10:08:04 -0700

    add backend url

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,3 +1,5 @@
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
 const debug = true;
 
-export const URL = debug ? "http://localhost:5000/api" : ""
+export const URL = debug ? `${BACKEND_URL}/api` : "http://localhost:5000/api";
