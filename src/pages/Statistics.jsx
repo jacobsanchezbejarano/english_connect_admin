f11eeccd5645f9e31dcea0920b49cf7a3f14547c
Jacob <jake_76847@hotmail.com>
2025-04-12 14:01:56 -0400

    Update navigation

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import StudentAttendanceChartData from '../components/StudentAttendanceChartData';
-import CountriesChartData from '../components/CountriesChartData';
-import CitiesChartData from '../components/CitiesChartData';
+// import CountriesChartData from '../components/CountriesChartData';
+// import CitiesChartData from '../components/CitiesChartData';
 import StakesChartData from '../components/StakesChartData';
 import WardsChartData from '../components/WardsChartData';
 import GroupsChartData from '../components/GroupsChartData';
@@ -19,16 +19,16 @@ function StatisticsPage() {
       <label htmlFor="level">Select Level:</label>
       <select id="level" value={level} onChange={handleLevelChange}>
         <option value="">Select</option>
-        <option value="countries">Countries</option>
-        <option value="cities">Cities</option>
+        {/* <option value="countries">Countries</option>
+        <option value="cities">Cities</option> */}
         <option value="stakes">Average Attendance by Group (Stake Level)</option>
         <option value="wards">Student's count by group (Stake Level)</option>
         <option value="groups">Group's number of sessions by Stake</option>
         <option value="students">Student's Attendance by Group</option>
       </select>
       {level === 'students' && <StudentAttendanceChartData />}
-      {level === 'countries' && <CountriesChartData />}
-      {level === 'cities' && <CitiesChartData />}
+      {/* {level === 'countries' && <CountriesChartData />} */}
+      {/* {level === 'cities' && <CitiesChartData />} */}
       {level === 'stakes' && <StakesChartData />}
       {level === 'wards' && <WardsChartData />}
       {level === 'groups' && <GroupsChartData />}
