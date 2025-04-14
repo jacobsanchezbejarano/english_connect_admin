ea5ed8fddb8f11c126652edadd1d9369adb2738e
Cesar Coutino <cou23001@byui.edu>
2025-04-14 00:35:25 -0700

    Remove interceptor from initial login

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import api from '../utils/axiosInstance'; // Use axios instance
-
+import axios from 'axios';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -9,47 +9,50 @@ export const AuthProvider = ({ children }) => {
   const [error, setError] = useState('');
 
   useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+  
     const fetchUserProfile = async () => {
       try {
-        const response = await api.get('/auth/profile'); // Uses axiosInstance
+        const response = await axios.get(`${BACKEND_URL}/api/auth/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         setUser(response.data.user);
       } catch (error) {
         console.error('Error validating token:', error);
-        localStorage.removeItem('accessToken'); // Token might be invalid
+        localStorage.removeItem('accessToken');
         setUser(null);
+        setError('Session expired. Please log in again.');
       } finally {
         setLoading(false);
       }
     };
-
-    const token = localStorage.getItem('accessToken');
+  
     if (token) {
       fetchUserProfile();
     } else {
       setLoading(false);
     }
   }, []);
+  
 
   const login = async (credentials) => {
     setError('');
   
     try {
-      const response = await api.post('/auth/login', {
+      const response = await axios.post(`${BACKEND_URL}/api/auth/login`, {
         email: credentials.username,
         password: credentials.password,
       });
   
-      if (response.data && response.data.accessToken) {
+      if (response.data) {
         localStorage.setItem('accessToken', response.data.accessToken);
         setUser(response.data.user);
         return true; // success
-      } else {
-        const msg = 'Login successful, but no token received.';
-        setError(msg);
-        throw new Error(msg);
-      }
+      } 
     } catch (error) {
-      const msg = error.response?.data?.message || 'Invalid credentials. Please try again.';
+      const msg = `${error.response?.data?.error}. Please try again` || error.response?.data?.message || `${error.message}. Please try again` || 'Invalid credentials. Please try again.';
       setError(msg);
       throw new Error(msg); // throw so caller can handle it
     }
