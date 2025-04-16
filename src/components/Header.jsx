102facd5b96efbc824484e745d77862b9d96eb42
Jacob <jake_76847@hotmail.com>
2025-04-15 20:39:29 -0400

    HIghlight active menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Logo from '../images/Logo.png';
 import { FaBars } from 'react-icons/fa';
 import { AiOutlineClose } from 'react-icons/ai';
@@ -9,6 +9,7 @@ import { useAuth } from '../context/authContext';
 const Header = () => {
   const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800);
   const { isAuthenticated, user } = useAuth();
+  const location = useLocation();
 
   useEffect(() => {
     const handleResize = () => {
@@ -33,29 +34,29 @@ const Header = () => {
       case 10:
         return (
           <>
-            <li><Link to="/attendance" onClick={closeNavHandler}>Attendance</Link></li>
-            <li><Link to="/users" onClick={closeNavHandler}>Users</Link></li>
-            <li><Link to="/students" onClick={closeNavHandler}>Students</Link></li>
-            <li><Link to="/instructors" onClick={closeNavHandler}>Instructors</Link></li>
-            <li><Link to="/groups" onClick={closeNavHandler}>Groups</Link></li>
-            <li><Link to="/stakes" onClick={closeNavHandler}>Stakes</Link></li>
-            <li><Link to="/units" onClick={closeNavHandler}>Wards & Branches</Link></li>
-            <li><Link to="/statistics" onClick={closeNavHandler}>Statistics</Link></li>
+            <li><Link to="/attendance" onClick={closeNavHandler} className={location.pathname === '/attendance' ? 'active' : ''}>Attendance</Link></li>
+            <li><Link to="/users" onClick={closeNavHandler} className={location.pathname === '/users' ? 'active' : ''}>Users</Link></li>
+            <li><Link to="/students" onClick={closeNavHandler} className={location.pathname === '/students' ? 'active' : ''}>Students</Link></li>
+            <li><Link to="/instructors" onClick={closeNavHandler} className={location.pathname === '/instructors' ? 'active' : ''}>Instructors</Link></li>
+            <li><Link to="/groups" onClick={closeNavHandler} className={location.pathname === '/groups' ? 'active' : ''}>Groups</Link></li>
+            <li><Link to="/stakes" onClick={closeNavHandler} className={location.pathname === '/stakes' ? 'active' : ''}>Stakes</Link></li>
+            <li><Link to="/units" onClick={closeNavHandler} className={location.pathname === '/units' ? 'active' : ''}>Wards & Branches</Link></li>
+            <li><Link to="/statistics" onClick={closeNavHandler} className={location.pathname === '/statistics' ? 'active' : ''}>Statistics</Link></li>
           </>
         );
       case 11:
         return (
           <>
-            <li><Link to="/attendance" onClick={closeNavHandler}>Attendance</Link></li>
-            <li><Link to="/students" onClick={closeNavHandler}>My Students</Link></li>
-            <li><Link to="/groups" onClick={closeNavHandler}>Groups</Link></li>
+            <li><Link to="/attendance" onClick={closeNavHandler} className={location.pathname === '/attendance' ? 'active' : ''}>Attendance</Link></li>
+            <li><Link to="/students" onClick={closeNavHandler} className={location.pathname === '/students' ? 'active' : ''}>My Students</Link></li>
+            <li><Link to="/groups" onClick={closeNavHandler} className={location.pathname === '/groups' ? 'active' : ''}>Groups</Link></li>
           </>
         );
       case 1:
         return (
           <>
-            <li><Link to="/my-attendance" onClick={closeNavHandler}>My Attendance</Link></li>
-            <li><Link to="/my-profile" onClick={closeNavHandler}>My Profile</Link></li>
+            <li><Link to="/my-attendance" onClick={closeNavHandler} className={location.pathname === '/my-attendance' ? 'active' : ''}>My Attendance</Link></li>
+            <li><Link to="/my-profile" onClick={closeNavHandler} className={location.pathname === '/my-profile' ? 'active' : ''}>My Profile</Link></li>
           </>
         );
       default:
@@ -66,19 +67,19 @@ const Header = () => {
   return (
     <nav>
       <div className='container nav__container'>
-        <Link to="/" className='nav__logo' onClick={closeNavHandler}>
+        <Link to="/" className={`nav__logo ${location.pathname === '/' ? 'active' : ''}`} onClick={closeNavHandler}>
           <img src={Logo} alt='Navbar Logo' />
         </Link>
         {isNavShowing && (
           <ul className='nav__menu'>
-            <li><Link to="/" onClick={closeNavHandler}>Home</Link></li>
+            <li><Link to="/" onClick={closeNavHandler} className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
 
             {isAuthenticated && renderMenuForRole()}
 
             {!isAuthenticated && (
               <>
-                <li><Link to="/register" onClick={closeNavHandler}>Register</Link></li>
-                <li><Link to="/login" onClick={closeNavHandler}>Login</Link></li>
+                <li><Link to="/register" onClick={closeNavHandler} className={location.pathname === '/register' ? 'active' : ''}>Register</Link></li>
+                <li><Link to="/login" onClick={closeNavHandler} className={location.pathname === '/login' ? 'active' : ''}>Login</Link></li>
               </>
             )}
 
